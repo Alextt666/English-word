fbb6d31f8c88121cd01040290672df08858e47af
alexzhang <alexzhangyaotian@gmail.com>
2023-06-07 11:59:04 +0800

    feat:路由跳转

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Dashbord from "@/views/Dashbord.vue";
+import Begin from "@/views/Begin.vue";
+import Help from "@/views/Help.vue";
 
-const routes = [{ path: "/", component: Dashbord }];
+const routes = [
+  { path: "/", redirect: "/General" },
+  { path: "/General", component: Dashbord },
+  { path: "/Begin", component: Begin },
+  { path: "/Help", component: Help },
+];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
 });
-
 
 export default router;
