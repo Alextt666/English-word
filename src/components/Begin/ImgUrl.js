26cb5c843c8ca03b549d3739a9688f39f183dd3e
alexzhang <alexzhangyaotian@gmail.com>
2023-06-08 15:21:59 +0800

    :poop: feat:翻翻卡初级版本

---
+++ src/components/Begin/ImgUrl.js
@@ -0,0 +1,12 @@
+const IMG_LIST = [
+    {
+      url: "https://th.bing.com/th/id/OIP.ibv6R9Kdgu9gStx8QKr2SgHaEo?w=242&h=180&c=7&r=0&o=5&pid=1.7",
+    },
+    {
+      url: "https://th.bing.com/th/id/OIP.DRQ0ps5tg5g4gc4Fc44UugHaEo?w=302&h=188&c=7&r=0&o=5&pid=1.7",
+    },
+    {
+      url: "https://th.bing.com/th/id/OIP.k7gIL351R3coaTPkNT0dUQHaEK?w=296&h=180&c=7&r=0&o=5&pid=1.7",
+    },
+  ];
+export {IMG_LIST} 
--- src/views/Begin.vue
+++ src/views/Begin.vue
@@ -1,11 +1,37 @@
 <template>
   <div class="begin-wrapper">
-    <div class="card-img"></div>
-    <div class="card-words"></div>
+    <div class="card-img">
+      <div>
+        <img class="card-img-item" :src="imgUrl" alt="img" />
+      </div>
+    </div>
+    <div class="card-words">
+      <button @click="nextWord">next</button>
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, computed } from "vue";
+import { IMG_LIST } from "@/components/Begin/imgUrl.js";
+const rotate = ref(0);
+const count = ref(0);
+const imgUrl = computed(() => IMG_LIST[count.value].url);
+// 下一张
+function nextWord() {
+
+  const cardImg = document.querySelector(".card-img");
+  const imgItem = document.querySelector(".card-img-item");
+  rotate.value = rotate.value == 0 ? 180 : 0;
+  if (count.value < IMG_LIST.length - 1) count.value++;
+  if (rotate.value == 180) {
+    imgItem.style.transform = `rotateY(180deg)`;
+  } else {
+    imgItem.style.transform = `rotateY(0deg)`;
+  }
+  cardImg.style.transform = `rotateY(${rotate.value}deg)`;
+}
+</script>
 
 <style lang="scss" scoped>
 .begin-wrapper {
@@ -22,6 +48,11 @@
     box-shadow: 0 0 10px #ccc;
     background-color: $bg-base;
     border-radius: 10px;
+    transition: all 3s;
+    &-item {
+      width: 3rem;
+      transition: all 5s;
+    }
   }
   .card-words {
     flex: 1;
