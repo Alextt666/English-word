592bbac5432554b03ca23b15387b489e87dd1af2
alexzhang <alexzhangyaotian@gmail.com>
2023-06-13 14:26:14 +0800

    :tada: feat:nextBtn

---
+++ src/components/Begin/NextBtn.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="next-btn flex-center" ref="nextbItem">
+    <img src="@/assets/images/next-word.svg" alt="" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const nextbItem = ref(null);
+const rightAnimate = () => {
+  nextbItem.value.style.transform = `translate(.3rem,.2rem)`;
+  setTimeout(() => {
+    nextbItem.value.style.transform = `translate(0rem,0rem)`;
+  }, 200);
+  return;
+};
+defineExpose({ rightAnimate });
+</script>
+
+<style lang="scss" scoped>
+  .next-btn {
+    position: absolute;
+    right: 10%;
+    bottom: 6%;
+    border-radius: 50%;
+    box-shadow: 0 0 10px #ccc;
+    width: 3rem;
+    height: 3rem;
+    cursor: pointer;
+    transition: all 0.5s;
+    img {
+      width: 2rem;
+    }
+  }
+  .next-btn:hover {
+    box-shadow: 0 0 10px #9e9d9d;
+  }
+</style>
--- src/views/Begin.vue
+++ src/views/Begin.vue
@@ -9,20 +9,25 @@
         }
       "
     ></CardImg>
-    <button @click="handleNext">next</button>
+    <NextBtn @click="handleNext" ref="nextb"></NextBtn>
   </div>
 </template>
 
 <script setup>
 import CardImg from "@/components/Begin/CardImg.vue";
+import NextBtn from "@/components/Begin/NextBtn.vue";
 import { useInitEffect } from "../components/Begin/useInit";
-
+import { ref } from "vue";
 // Blur-Card相关全部逻辑
 // 需要共用同一个示例
 const { nextWord, imgUrl, word, checkAnswer } = useInitEffect();
+const nextb = ref(null);
+
+
 
 // 下一张
 const handleNext = () => {
+  nextb.value.rightAnimate();
   nextWord();
   return;
 };
@@ -36,5 +41,6 @@ const handleNext = () => {
   height: 68%;
   justify-content: center;
   transition: 1s all;
+  position: relative;
 }
 </style>
